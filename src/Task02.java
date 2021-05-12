a9cd8e4ea0f4585546e76b05a4d08988ee53dbdb
mrkirillb <mrkirillb@gmail.com>
2021-05-12 21:45:03 +0300

    Выполняем задание №2

---
+++ src/Task02.java
@@ -0,0 +1,9 @@
+public class Task02 {
+    public static void main(String[] args) {
+        double a = 2.97;
+        System.out.println(a);  //вывод в консоль сделал для полноты картины
+
+        byte b = 127;
+        System.out.println(b);  //вывод в консоль сделал для полноты картины
+    }
+}

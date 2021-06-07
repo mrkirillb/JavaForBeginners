e902aacb12b1d72cbd3007571500cf6a6706eb4d
mrkirillb <mrkirillb@gmail.com>
2021-06-07 12:15:07 +0300

    Изменения в Task14

--- src/Task14.java
+++ src/Task14.java
@@ -1,30 +1,18 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task14 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] someArray = new int[15];
-
-        //Выводим все элементы массива
-        for (int i = 0; i < someArray.length; i++) {
-            int result = 20 - random.nextInt(40);
-            someArray[i] = result;
-            System.out.print(result+"; ");
+        int[] mas = new int[15];
+        int max = 20;
+        int min = -20;
+        for (int i =0; i < mas.length; i++){
+            mas[i] = (int)(Math.random() * ((max - min) + 1)) + min;
         }
-        System.out.println();
-
-        //Выводим максимальный и минимальный элемент
-        int max = someArray[0];
-        int min = someArray[0];
-        for (int k=0;k<someArray.length;k++){
-            max = Math.max(max,someArray[k]);
-            min = Math.min(min,someArray[k]);
-        }
-        System.out.print("Максимальный элемент массива: ");
-        System.out.println(max);
-        System.out.print("Минимальный элемент массива: ");
-        System.out.println(min);
-        System.out.print("Наибольший элемент по модулю: ");
-        System.out.println(Math.max(Math.abs(max),Math.abs(min)));
+        Arrays.sort(mas);
+        System.out.println("Максимальный элемент " + mas[mas.length-1]);
+        System.out.println("Минимальный элемент " + mas[0]);
+        System.out.println("Максимальный элемент по модулю " + Math.max((Math.abs(mas[mas.length-1])),(Math.abs(mas[0]))));
     }
 }
